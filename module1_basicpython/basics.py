75959b2b834d541f9e978d147b950492d8f53da5
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-07-27 22:10:54 +0530

    completed variables and datatypes in module1

---
+++ module1_basicpython/basics.py
@@ -0,0 +1,14 @@
+#inbuild function
+print("hello world")    
+a=15
+print(a)
+print (type(a))
+
+
+b=2         #int
+c=2.3       #float
+d='A'       #string
+e='python'  #string
+f=True      #boolean
+
+print(type(b), type(c), type(d), type(e), type(f))
