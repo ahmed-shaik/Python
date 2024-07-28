da4f9fde155e60f40cd826f2d530980e6cc6101b
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-07-28 22:29:23 +0530

    completed till string formatting

--- module1_basicpython/basics2.py
+++ module1_basicpython/basics2.py
@@ -1,10 +1,42 @@
 #Input Function
 
-a=int(input())
-print(type(a))
-b=int(input())
-print(type(b))
-print(a+b)
+# a=int(input())
+# print(type(a))
+# b=int(input())
+# print(type(b))
+# print(a+b)
+
+# condition statements
+
+# a=10
+# if a>5:
+#     print("a is greater than 5")
+# else:
+#     print("a is not greater than 5")
+
+
+# Take weather condition from the user
+# sunny - Summer Season
+# cold - winter se son
+# rain - Monsoon season
+# anything else - Wrong input
+
+# a=input()
+# if a=="sunny":
+#     print("summer")
+# elif a=="cold":
+#     print("winter")
+# elif a=="rain":
+#     print("mansoon")
+# else:
+#     print("wrong input")
+    
 # String Formatting
+
+a=55
+print(a, "is a number")
+print(str(a)+ " is a number")
+print(f"{a} is a number")
+
 # Condition
 # Loop
