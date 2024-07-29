9002b21342ea7cbe96bd8e8c92b13f238e692731
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-07-29 22:08:50 +0530

    completed loops

--- module1_basicpython/basics2.py
+++ module1_basicpython/basics2.py
@@ -34,12 +34,54 @@
     
 # String Formatting
 
-a=55
-print(a, "is a number")
-print(str(a)+ " is a number")
-print(f"{a} is a number")
-
-
+# a=55
+# print(a, "is a number")
+# print(str(a)+ " is a number")
+# print(f"{a} is a number")
 
 
 # Loop
+# while loop
+# i=1
+# while i<5:
+#     print(i)
+#     i+=1
+
+# for loop
+# range start, stop[end], step
+# for i in range(100):
+#     print(i, end=' ')
+# Multiplicatlon table of a given number using for loop
+# for i in range(1,11):
+#     print(f'5 * {i} = {i*5}')
+
+# List/ Tuple
+# a=[1,2,3,4,5,6,7,8,9,10] # Is Mutable object
+# print(a)
+# print(type(a))
+# print(len(a))
+# print(a[1])
+
+# b = (77, 76, 75, 74)
+# print(b)
+# print(type(b))
+
+# list_demo = [999, 'Python' , 12.3, True, "Hello"]
+# for i in list_demo:
+#     print(f'{i} is belonging to {type(i)}')
+
+# print(list(range(5)))
+
+# Quotes
+# We can Single Double Triple in Python
+# use single quote/Doub1e Quote/ Triple Quote for initializing a String
+# Single Quote - character / word
+# Double Quote - Word/ Sentence 
+# Triple Quote - Sentence/paragrapd
+
+# Assignment Use both the loops and print the even values from 40 to 0
+i=40
+while(i>=0):
+    if(i%2==0):
+        print(i, end=" ")
+    i-=1
