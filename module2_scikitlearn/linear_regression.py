6e281d766587bdf4e07c1fe7258807591b75f4fc
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-04 21:42:35 +0530

    started linear regression

---
+++ module2_scikitlearn/linear_regression.py
@@ -0,0 +1,8 @@
+# Linear Models - Regressors in Scikit-Learn
+# For linear models, there are many estimator classes which are very close to each other. Let us have a look at
+# • LinearRegression, no penalty
+# • Ridge, L2 penalty
+# • Lasso, LI penalty (sparse models)
+# • ElasticNet, L1 + L2 penalty (less sparse models)
+# • SGDRegressor With loss='squared_loss'
+
