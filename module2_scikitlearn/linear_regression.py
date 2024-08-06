a080170bc2359355a68a76b4c578e1e460a5827a
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-06 20:50:13 +0530

    linear regression

--- module2_scikitlearn/linear_regression.py
+++ module2_scikitlearn/linear_regression.py
@@ -6,3 +6,13 @@
 # • ElasticNet, L1 + L2 penalty (less sparse models)
 # • SGDRegressor With loss='squared_loss'
 
+import pandas as pd
+df = pd.read_csv('C:\\Users\\ahmed\\Downloads\\Salary_dataset.csv')
+print(df.head())
+
+import matplotlib.pyplot as plt
+plt.scatter(df['YearsExperience'] , df['Salary'])
+plt.title("iScatter Plo")
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt. show()
