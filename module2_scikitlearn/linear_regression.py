2b1012ac6533aff99c06eb290d4ff8ae2a12b8b5
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-07 20:40:55 +0530

    completed linear regression

--- module2_scikitlearn/linear_regression.py
+++ module2_scikitlearn/linear_regression.py
@@ -8,15 +8,26 @@
 
 import pandas as pd
 df = pd.read_csv('C:\\Users\\ahmed\\Downloads\\Salary_dataset.csv')
-print(df.head())
+# print(df.head())
 
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+# plt.scatter(df['YearsExperience'] , df['Salary'])
+# plt.title("iScatter Plo")
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
+# plt. show()
+x = df.iloc[:,0].values.reshape(-1, 1)
+y = df.iloc[:,1].values
+# print(x)
+reg=LinearRegression().fit(x, y)
+print(reg.coef_)
+
+# PLotting the best fit line
+y_pred = reg.predict(x)
 plt.scatter(df['YearsExperience'] , df['Salary'])
+plt.plot(df['YearsExperience'] , y_pred)
 plt.title("iScatter Plo")
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt. show()
-x = df.iloc[:,0].values()
-y = df.iloc[:,1].values()
-reg=LinearRegression().fit(x, y)
-print(reg)
