1e0f72a9122eeb4e1e71b02952489f36f20428da
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-16 19:33:54 +0530

    completed logistic regression

--- module2_scikitlearn/logistic_regression.py
+++ module2_scikitlearn/logistic_regression.py
@@ -21,7 +21,19 @@ from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
 # print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+
+
 from sklearn.linear_model import LogisticRegression
 
 clf = LogisticRegression(random_state=0).fit(x_train, y_train)
-print(clf.predict(x_test))
+y_pred = clf.predict(x_test)
+
+from sklearn.metrics import confusion_matrix,accuracy_score
+print(confusion_matrix(y_test,y_pred))
+print(accuracy_score(y_test,y_pred)*100)
