1849ca99bd4897cbdf706c08bed3e50cf349f8a0
ahmed-shaik <ahmedshaikvk18@gmail.com>
2024-08-16 19:15:23 +0530

    logistic regression

--- module2_scikitlearn/logistic_regression.py
+++ module2_scikitlearn/logistic_regression.py
@@ -7,8 +7,21 @@ df = pd.read_csv('C:\\Users\\ahmed\\Downloads\\Social_Network_Ads.csv')
 # plt.scatter(df['Age'],df['EstimatedSalary'])
 # plt.show()
 
-p_df = df[df['Purchased']==1]
-np_df = df[df['Purchased']==0]
-plt.scatter(p_df['Age'], p_df['EstimatedSalary'])
-plt.scatter(np_df['Age'], np_df['EstimatedSalary'])
-plt.show()
+# p_df = df[df['Purchased']==1]
+# np_df = df[df['Purchased']==0]
+# plt.scatter(p_df['Age'], p_df['EstimatedSalary'], label="Purchased")
+# plt.scatter(np_df['Age'], np_df['EstimatedSalary'], label="Not Purchased")
+# plt.legend()
+# plt.show()
+
+x=df[['Age', 'EstimatedSalary']].values
+y=df['Purchased'].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+# print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
+
+from sklearn.linear_model import LogisticRegression
+
+clf = LogisticRegression(random_state=0).fit(x_train, y_train)
+print(clf.predict(x_test))
